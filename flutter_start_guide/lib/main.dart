d92174f3ec892686126bacad57500dabdd6d08b3
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-03-21 13:54:17 +0545

    Created new Question widget to represent the questions

--- flutter_start_guide/lib/main.dart
+++ flutter_start_guide/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -9,22 +11,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
   final questionList = [
     'What your mother\'s name?',
     'What is your brother\'s name?',
   ];
 
-  void ansewerQuestionPressed() {
+  void _answerQuestionPressed() {
     setState(() {
-      questionIndex++;
+      _questionIndex++;
     });
-    print('Answer pressed is ' + questionIndex.toString());
+    print('Answer pressed is ' + _questionIndex.toString());
   }
 
   @override
@@ -38,18 +40,18 @@ class MyAppState extends State<MyApp> {
           children: <Widget>[
             Text('The question is: '),
             RaisedButton(
-              child: Text(
-                questionList[questionIndex % 2],
+              child: QuestionWidget(
+                questionList[_questionIndex % 2],
               ),
-              onPressed: ansewerQuestionPressed,
+              onPressed: _answerQuestionPressed,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: ansewerQuestionPressed,
+              onPressed: _answerQuestionPressed,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: ansewerQuestionPressed,
+              onPressed: _answerQuestionPressed,
             ),
           ],
         ),
