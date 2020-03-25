e9063d0c212f99360661ce369daee533ee9d7254
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-03-25 19:44:55 +0545

    updatd logic to allow text to be entered when the soft keyboard is displayed

--- flutter_expense_tracker/lib/widgets/new_transaction.dart
+++ flutter_expense_tracker/lib/widgets/new_transaction.dart
@@ -53,56 +53,63 @@ class _NewTransactionWidgetState extends State<NewTransactionWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 6,
-      child: Container(
-        color: Colors.indigo[50],
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Title',
-              ),
-              controller: _titleController,
-              onSubmitted: (_) => _submitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Amount',
-              ),
-              controller: _amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitData(),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  // causes text to take all the free space which pushes the button below to the right
-                  child: Text(
-                    _userSelectedDatetime == null
-                        ? 'No Date Selected'
-                        : 'Selected Date: ${DateFormat.yMd().format(_userSelectedDatetime)}',
-                  ),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 6,
+        child: Container(
+          color: Colors.indigo[50],
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
                 ),
-                FlatButton(
-                  textColor: Theme.of(context).primaryColor,
-                  child: Text(
-                    'Select Date',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: _displayDatePicker,
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Amount',
                 ),
-              ],
-            ),
-            RaisedButton(
-              onPressed: _submitData,
-              child: Text('Add Transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-            )
-          ],
+                controller: _amountController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitData(),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    // causes text to take all the free space which pushes the button below to the right
+                    child: Text(
+                      _userSelectedDatetime == null
+                          ? 'No Date Selected'
+                          : 'Selected Date: ${DateFormat.yMd().format(_userSelectedDatetime)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Select Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _displayDatePicker,
+                  ),
+                ],
+              ),
+              RaisedButton(
+                onPressed: _submitData,
+                child: Text('Add Transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+              )
+            ],
+          ),
         ),
       ),
     );
