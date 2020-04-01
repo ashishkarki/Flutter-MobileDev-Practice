e72da4937dd51a4559cde8829e4fca53f957045b
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-04-01 07:46:08 +0545

    upgraded the orders_screen to enable expanding and collapsing of order items

--- flutter_shopping_app/lib/screens/orders_screen.dart
+++ flutter_shopping_app/lib/screens/orders_screen.dart
@@ -19,7 +19,28 @@ class OrdersScreen extends StatelessWidget {
       drawer: AppDrawerWidget(),
       body: ordersProvider.orders.length == 0
           ? Card(
-              child: Text('No orders yet..'),
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                  vertical: 50,
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.orange,
+                  width: 3,
+                )),
+                child: Card(
+                  elevation: 3,
+                  child: Text(
+                    'No orders yet. Please use the Products Screen to order some.',
+                    style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize: 30,
+                      //backgroundColor: Colors.black45,
+                    ),
+                  ),
+                ),
+              ),
             )
           : ListView.builder(
               itemBuilder: (ctx, itemIdx) => OrderItemWidget(
--- flutter_shopping_app/lib/widgets/order_item.dart
+++ flutter_shopping_app/lib/widgets/order_item.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders_provider.dart';
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem orderItem;
 
   OrderItemWidget(this.orderItem);
+
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  var _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +24,61 @@ class OrderItemWidget extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${orderItem.amount}'),
+            title: Text('\$${widget.orderItem.amount}'),
             subtitle: Text(
-              DateFormat('MM dd yyyy hh:mm').format(orderItem.dateTime),
+              DateFormat('MM dd yyyy hh:mm').format(widget.orderItem.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(
+                _isExpanded ? Icons.expand_less : Icons.expand_more,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_isExpanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 5,
+              ),
+              height: min(
+                (widget.orderItem.cartProducts.length * 20.0 + 50.0),
+                150.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black45,
+                  width: 3,
+                ),
+              ),
+              child: ListView(
+                children: widget.orderItem.cartProducts
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${product.quantity} x \$${product.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
