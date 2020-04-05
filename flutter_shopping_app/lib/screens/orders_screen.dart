7f50ef5a4ab4adb2711385414f0ed2b765d96d7d
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-04-05 16:30:56 +0545

    using FutureBuilder syntax in the orders_screen widget

--- flutter_shopping_app/lib/screens/orders_screen.dart
+++ flutter_shopping_app/lib/screens/orders_screen.dart
@@ -5,78 +5,69 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders_provider.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    //Future.delayed(Duration.zero).then((_) async {
-    //setState(() {
-    _isLoading =
-        true; // setState not needed since init runs before build and sets the state
-    //});
-    // actually with listen: false, we don't need Future.deleyed within init. but, anyways.
-    Provider.of<OrdersProvider>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-
-    //});
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<OrdersProvider>(context);
+    // final ordersProvider = Provider.of<OrdersProvider>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawerWidget(),
-      body: ordersProvider.orders.length == 0
-          ? Card(
-              child: Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 25,
-                  vertical: 50,
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.orange,
-                  width: 3,
-                )),
-                child: Card(
-                  elevation: 3,
-                  child: Text(
-                    'No orders yet. Please use the Products Screen to order some.',
-                    style: TextStyle(
-                      color: Colors.orangeAccent,
-                      fontSize: 30,
-                      //backgroundColor: Colors.black45,
-                    ),
-                  ),
-                ),
-              ),
-            )
-          : _isLoading
-              ? CircularProgressIndicator()
-              : ListView.builder(
-                  itemBuilder: (ctx, itemIdx) => OrderItemWidget(
-                    ordersProvider.orders[itemIdx],
-                  ),
-                  itemCount: ordersProvider.orders.length,
-                ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawerWidget(),
+        body: FutureBuilder(
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                return Center(
+                  child: Text('error occurred while fetching your orders'),
+                );
+              } else {
+                return Consumer<OrdersProvider>(
+                  builder: (ctx, ordersProvider, child) =>
+                      ordersProvider.orders.length == 0
+                          ? Card(
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 25,
+                                  vertical: 50,
+                                ),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                  color: Colors.orange,
+                                  width: 3,
+                                )),
+                                child: Card(
+                                  elevation: 3,
+                                  child: Text(
+                                    'No orders yet. Please use the Products Screen to order some.',
+                                    style: TextStyle(
+                                      color: Colors.orangeAccent,
+                                      fontSize: 30,
+                                      //backgroundColor: Colors.black45,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : ListView.builder(
+                              itemBuilder: (ctx, itemIdx) => OrderItemWidget(
+                                ordersProvider.orders[itemIdx],
+                              ),
+                              itemCount: ordersProvider.orders.length,
+                            ),
+                );
+              }
+            }
+          },
+          future: Provider.of<OrdersProvider>(context, listen: false)
+              .fetchAndSetOrders(),
+        ));
   }
 }
