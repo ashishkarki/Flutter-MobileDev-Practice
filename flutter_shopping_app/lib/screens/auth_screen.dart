6b324b9601fc137ff134c721204d23e3cbe56723
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-04-16 15:02:37 +0545

    Implemented Manual height animation between login and signup screens

--- flutter_shopping_app/lib/screens/auth_screen.dart
+++ flutter_shopping_app/lib/screens/auth_screen.dart
@@ -95,7 +95,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -104,6 +105,40 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _animationCtrl;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationCtrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationCtrl,
+        curve: Curves.linear,
+      ),
+    );
+    // empty setState to trigger build method to update the screen when height changes
+    _heightAnimation.addListener(
+      () => setState(
+        () {},
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _animationCtrl.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -181,11 +216,13 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animationCtrl.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
     }
+    _animationCtrl.reverse();
   }
 
   @override
@@ -197,9 +234,12 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          // minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+          minHeight: _heightAnimation.value.height,
+        ),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
