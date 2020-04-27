3f78d278795c2c366b27104313508b3cd536474f
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-04-27 17:44:01 +0545

    finished implementing a very simple magic 8 ball app

---
+++ flutter_magic_8_ball/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Second-program-files\Google\Flutter SDK\flutter_windows_v1.12.13+hotfix.8-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\ASHISH-KARKI_FOLDERS\ashish-temp-disposable\Flutter-study\Flutter-MobileDev-Practice\flutter_magic_8_ball\magic-8-ball-flutter-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=D:\Second-program-files\Google\Flutter SDK\flutter_windows_v1.12.13+hotfix.8-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
